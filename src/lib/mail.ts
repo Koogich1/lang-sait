299a39559fb917a149cea57d9904554e06fcd909
Koogich1 <101112692+Koogich1@users.noreply.github.com>
2024-08-03 13:56:31 +0300

    value

--- src/lib/mail.ts
+++ src/lib/mail.ts
@@ -13,13 +13,13 @@ export const sendTwoFactorTokenEmail = async (
 		html: `<p>Ваш код: ${token}</p>`
 	})
 }
-	const domain = "http://send.acyberg.com:3000"
+	const domain = process.env.NEXT_PUBLIC_APP_URL
 
 export const sendPasswordResetEmail = async (
 	email: string,
 	token: string,
 ) => {
-	const resetLink = `http://send.acyberg.com/auth/new-password?token=${token}`
+	const resetLink = `${domain}/auth/new-password?token=${token}`
 	await resend.emails.send({
 		from: "mail@acyberg.com",
 		to: email,
@@ -32,7 +32,7 @@ export const sendVerificationEmail = async (
 	email: string,
 	token: string
 ) => {
-	const confirmLink = `http://send.acyberg.com/auth/new-verification?token=${token}`
+	const confirmLink = `${domain}/auth/new-verification?token=${token}`
 
 	await resend.emails.send({
 		from: "mail@acyberg.com",
