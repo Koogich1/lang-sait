0f9e84b899fafd5d6edd1a5dcdebe9203350f3f0
Koogich1 <101112692+Koogich1@users.noreply.github.com>
2024-08-03 10:43:10 +0300

    fsad

--- src/lib/mail.ts
+++ src/lib/mail.ts
@@ -13,7 +13,7 @@ export const sendTwoFactorTokenEmail = async (
 		html: `<p>Ваш код: ${token}</p>`
 	})
 }
-	const domain = "http://send.acyberg.com:3000"
+	const domain = process.env.NEXT_PUBLIC_APP_URL
 
 export const sendPasswordResetEmail = async (
 	email: string,
@@ -32,7 +32,7 @@ export const sendVerificationEmail = async (
 	email: string,
 	token: string
 ) => {
-	const confirmLink = `http://localhost:3000/auth/new-verification?token=${token}`
+	const confirmLink = `${domain}/auth/new-verification?token=${token}`
 
 	await resend.emails.send({
 		from: "mail@acyberg.com",
