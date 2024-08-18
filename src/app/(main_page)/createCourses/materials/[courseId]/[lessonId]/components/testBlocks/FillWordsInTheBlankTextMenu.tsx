ef94cb64afcdd82e0cdd11c41c0efe90a8377e6e
Koogich1 <101112692+Koogich1@users.noreply.github.com>
2024-08-18 20:32:24 +0300

    errors

--- src/app/(main_page)/createCourses/materials/[courseId]/[lessonId]/components/testBlocks/FillWordsInTheBlankTextMenu.tsx
+++ src/app/(main_page)/createCourses/materials/[courseId]/[lessonId]/components/testBlocks/FillWordsInTheBlankTextMenu.tsx
@@ -27,15 +27,13 @@ interface AudioChooseProps {
 const FillWordsInTheBlankTextMenu: React.FC<AudioChooseProps> = ({ test, userId, courseUserId, updateVisov }) => {
     const parts = test.question.split(/(\[\])/g);
     
-    // Группировка опций по порядку
-
     const groupOptionsByOrder = () => {
         const groupedOptions: { [key: number]: Option[] } = {};
         test.options.forEach(option => {
-					if(!option.order){
-						return
-					}
-            const order = option.order; // Используем order напрямую
+            if (!option.order) {
+                return;
+            }
+            const order = option.order;
             if (!groupedOptions[order]) {
                 groupedOptions[order] = [];
             }
@@ -45,31 +43,28 @@ const FillWordsInTheBlankTextMenu: React.FC<AudioChooseProps> = ({ test, userId,
     };
 
     const groupedOptions = groupOptionsByOrder();
-
-    // Переменная для отслеживания текущего порядка
     let currentOrder = 1;
 
     const data = parts.map((part, idx) => {
         if (part === '[]') {
-            const optionsForThisBlank = groupedOptions[currentOrder] || []; // Получаем опции для текущего порядка
-
-            currentOrder++; // Увеличиваем order для следующего пустого места
+            const optionsForThisBlank = groupedOptions[currentOrder] || []; 
+            currentOrder++;
 
             return (
-							<DropdownMenu>
-								<DropdownMenuTrigger asChild>
-									<button className="px-2 py-1 border-2 border-blue-400 bg-blue-100 font-semibold text-blue-500 rounded-lg hover:bg-blue-200 transition-all">
-										Выберите...
-									</button>
-								</DropdownMenuTrigger>
-								<DropdownMenuContent className="bg-white flex gap-2">
-                  {optionsForThisBlank.map((option, optionIdx) => (
-                    <DropdownMenuItem key={optionIdx} className={`px-2 py-1 font-semibold border-2 ${option.isCorrect ? "border-green-400 hover:text-green-500 hover:bg-green-200 bg-green-100": "border-red-400 hover:text-red-500 hover:bg-red-200 bg-red-100"}`}>
-                      {option.text} {/* Отображаем текст каждого варианта */}
-                    </DropdownMenuItem>
-                  ))}
-								</DropdownMenuContent>
-							</DropdownMenu>
+                <DropdownMenu key={idx}>
+                    <DropdownMenuTrigger asChild>
+                        <button className="px-2 py-1 border-2 border-blue-400 bg-blue-100 font-semibold text-blue-500 rounded-lg hover:bg-blue-200 transition-all">
+                            Выберите...
+                        </button>
+                    </DropdownMenuTrigger>
+                    <DropdownMenuContent className="bg-white flex gap-2">
+                        {optionsForThisBlank.map((option) => (
+                            <DropdownMenuItem key={option.id} className={`px-2 py-1 font-semibold border-2 ${option.isCorrect ? "border-green-400 hover:text-green-500 hover:bg-green-200 bg-green-100": "border-red-400 hover:text-red-500 hover:bg-red-200 bg-red-100"}`}>
+                                {option.text}
+                            </DropdownMenuItem>
+                        ))}
+                    </DropdownMenuContent>
+                </DropdownMenu>
             );
         } else {
             return (
@@ -82,15 +77,11 @@ const FillWordsInTheBlankTextMenu: React.FC<AudioChooseProps> = ({ test, userId,
 
     return (
         <div>
-					<div className="flex items-center justify-between">
-						<h1 className="text-lg font-semibold text-[#6647a4]">
-							{test.audioHeader}
-						</h1>
-                        <UpdateFillWordsMultibleText test={test} updateVisov={() => updateVisov()}/>
-					</div>
-          <div className="mt-3">
-						{data}
-					</div>
+            <div className="flex items-center justify-between">
+                <h1 className="text-lg font-semibold text-[#6647a4]">{test.audioHeader}</h1>
+                <UpdateFillWordsMultibleText test={test} updateVisov={() => updateVisov()} />
+            </div>
+            <div className="mt-3">{data}</div>
         </div>
     );
 };
