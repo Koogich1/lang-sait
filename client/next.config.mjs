d7219983d9bba0f2566adb8d6e892beb1bbcf397
Koogich1 <101112692+Koogich1@users.noreply.github.com>
2024-12-05 17:24:32 +0300

    fff

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -11,7 +11,7 @@ const nextConfig = {
     ]
   },
   experimental:{
-    serverComponentsExternalPackages: ['sharp'],
+    serverExternalPackages: ['sharp'],
     serverActions: {
       bodySizeLimit: "5mb",
     }
